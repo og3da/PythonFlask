30b35fab4ff4ea70bcb21f99d1309c127aff3028
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-07-20 17:12:08 -0300

    implementando POST hotel

--- secao6_API_REST_CRUD/resources/hotel.py
+++ secao6_API_REST_CRUD/resources/hotel.py
@@ -2,7 +2,7 @@
 na aula 35.Refatorando, nós criamos o pacote "Resources" e incluimos esse arquivo "hotel.py"
 com a Classe "Hoteis" que é um recurso, usado em app.py
 """
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -40,7 +40,25 @@ class Hotel(Resource):
         return {'message': 'hotel not found'}, 404 # status code not found
 
     def post(self, hotel_id):
-        pass
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade'],
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
     def put(self, hotel_id):
         pass
     def delete(self, hotel_id):
