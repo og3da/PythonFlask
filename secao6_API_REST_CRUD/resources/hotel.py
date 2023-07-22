0447e149a9c38c66b24cd7b91685b55183e5a82e
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-07-22 12:00:51 -0300

    criando classe modelo para hotel
    nessa aula criamos a classe "HotelModel" e uma pasta específica para os "models". 
    Passamos a usar essa classe em "resources/hotel" para converter em json os argumentos

--- secao6_API_REST_CRUD/resources/hotel.py
+++ secao6_API_REST_CRUD/resources/hotel.py
@@ -3,6 +3,7 @@ na aula 35.Refatorando, nós criamos o pacote "Resources" e incluimos esse arqui
 com a Classe "Hoteis" que é um recurso, usado em app.py
 """
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -55,14 +56,16 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
