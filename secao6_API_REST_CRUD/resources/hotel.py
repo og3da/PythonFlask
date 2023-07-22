3b8d45a8ada9226893b109532c1e11eb7c403e3a
Raphael Ogeda <71846924+og3da@users.noreply.github.com>
2023-07-22 00:23:50 -0300

    implementando PUT / DELETE

--- secao6_API_REST_CRUD/resources/hotel.py
+++ secao6_API_REST_CRUD/resources/hotel.py
@@ -28,38 +28,54 @@ hoteis = [
     }
 ]
 
+
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
+
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas', type=float)
+    argumentos.add_argument('diaria', type=float)
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'hotel not found'}, 404 # status code not found
 
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('cidade')
-
-        dados = argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'cidade': dados['cidade'],
-        }
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
 
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
-        pass
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        else:
+            hoteis.append(novo_hotel)
+            return novo_hotel, 201
+
     def delete(self, hotel_id):
-        pass
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hoteis.remove(hotel)
+            return {'message': 'hotel deleted'}, 200
+        else:
+            return {'message': 'hotel not found'}, 404
